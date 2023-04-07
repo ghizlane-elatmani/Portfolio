5efaa36e633f6852db8dafe400e84c5784086d52
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-04-07 16:55:29 +0000

    1.8.1: Add weather project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,4 @@
+import weather from "../assets/weather.png";
 import quiz from "../assets/quiz.png";
 import todo from "../assets/todo.png";
 import wind from "../assets/windbnb.png";
@@ -6,22 +7,30 @@ import recipe from "../assets/recipe.png";
 export const projectList = [
   {
     id: "1",
-    name: "CountryQuiz",
+    name: "Weather App",
+    desc: "Get the weather forecast in a beautiful website",
+    urlSite: "https://weather-app-ge.netlify.app/",
+    urlCode: "https://github.com/ghizlane-elatmani/Weather-app",
+    picture: weather,
+  },
+  {
+    id: "2",
+    name: "Country Quiz",
     desc: "Application display a quiz about countries and their capital",
     urlSite: "https://country-quiz-ge.netlify.app/",
     urlCode: "https://github.com/ghizlane-elatmani/Country-quiz",
     picture: quiz,
   },
   {
-    id: "2",
-    name: "TodoApp",
+    id: "3",
+    name: "Todo App",
     desc: "A modern and simple task management app",
     urlSite: "https://todo-app-ge.netlify.app/",
     urlCode: "https://github.com/ghizlane-elatmani/Todo-App",
     picture: todo,
   },
   {
-    id: "3",
+    id: "4",
     name: "WindBnb",
     desc: "Application that displays a simple version of airbnb",
     urlSite: "https://windbnb-ge.netlify.app/",
@@ -29,7 +38,7 @@ export const projectList = [
     picture: wind,
   },
   {
-    id: "3",
+    id: "5",
     name: "Recipe Blog",
     desc: "A homepage designed for a recipe blog",
     urlSite: "https://recipe-blog-ge.netlify.app/",
